a5e5172e6886b4b0e3aae72a929455969576031e
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2024-02-20 14:35:17 +0200

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import Table from './components/Table/Table';
 import BoxThema from './components/Context/BoxThema';
 import { ThemeProvider } from './components/Context/ThemaContext';
 import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher';
-
+import { InlineWidget } from 'react-calendly';
 function App() {
   return (
-    <ThemeProvider>
-      <BoxThema>
-        <ThemeSwitcher />
-        <h1 style={{ textAlign: 'center', marginBottom: '50px' }}>
-          Таблиця з даними про Котиків
-        </h1>
-        <Table />
-      </BoxThema>
-    </ThemeProvider>
+    // <ThemeProvider>
+    //   <BoxThema>
+    //     <ThemeSwitcher />
+    //     <h1 style={{ textAlign: 'center', marginBottom: '50px' }}>
+    //       Таблиця з даними про Котиків
+    //     </h1>
+    //     <Table />
+    //   </BoxThema>
+    // </ThemeProvider>
+    <div>
+      <InlineWidget url="https://calendly.com/vladislavshsh" />
+    </div>
   );
 }
 
